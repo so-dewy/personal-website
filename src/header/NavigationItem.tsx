1817af4679e997fe8d2f59ae35fd15801b7ce1fd
roman-azami <nascentsoul13@gmail.com>
2021-04-03 02:55:15 +0300

    Style NavigationItem

--- src/header/NavigationItem.tsx
+++ src/header/NavigationItem.tsx
@@ -1,11 +1,18 @@
 import {
   Link
 } from "react-router-dom";
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  color: palevioletred;
+  text-decoration: none;
+  margin: 1rem;
+`;
 
 function NavigationItem(props: { path: string, text: string }) {
   return (
     <li>
-      <Link to={props.path}>{props.text}</Link>
+      <StyledLink to={props.path}>{props.text}</StyledLink>
     </li>
   );
 }
