031925b248fabd0973e625be5e7428551aae1a86
roman-azami <nascentsoul13@gmail.com>
2021-04-03 14:50:06 +0300

    Add name-logo to header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -4,11 +4,16 @@ import styled from 'styled-components';
 const StyledHeader = styled.header`
   min-height: 8vh;
   font-size: calc(8px + 2vmin);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
 
 function Header() {
   return (
     <StyledHeader>
+      Roman Azami
       <NavigationList />
     </StyledHeader>
   );
